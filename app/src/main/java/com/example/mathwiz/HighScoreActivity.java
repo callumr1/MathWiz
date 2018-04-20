0c0688f8c66d0cd42a2adf52301cf0b5a1800501
Callum Robertson <callumr1@users.noreply.github.com>
2018-04-20 15:27:55 +1000

    Initial commit

---
+++ app/src/main/java/com/example/mathwiz/HighScoreActivity.java
@@ -0,0 +1,13 @@
+package com.example.mathwiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score);
+    }
+}
